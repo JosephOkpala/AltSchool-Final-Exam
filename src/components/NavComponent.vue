5eebcac12eb4d489ecb4f4df91d3a570a8ee25aa
JosephOkpala <okpalajoseph995@gmail.com>
2023-03-14 18:56:08 +0100

    navbar style and markup

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,24 +1,43 @@
 <template>
-    <div>
-        <nav>
-            <img alt="Vue logo" src="../assets/logo.png" class="logo" />
-            <div>
-                <router-link to="/">Home</router-link>
-                <router-link to="/repos">Repos</router-link>
-                <router-link to="/profile">Profile</router-link>
-            </div>
-        </nav>
-    </div>
+<header>
+    <nav>
+        <img alt="Vue logo" src="../assets/logo.png" class="logo" />
+        <div class="nav-lemps">
+            <router-link to="/">Home</router-link>
+            <router-link to="/repos">Repos</router-link>
+            <router-link to="/profile">Profile</router-link>
+        </div>
+    </nav>
+</header>
 </template>
 
 <script>
-export default{
-    name : 'NavComponent',
+export default {
+    name: 'NavComponent',
 }
 </script>
 
 <style scoped>
-.logo{
-width: 5rem;
+nav {
+    /* border: 1px solid red; */
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 1rem 0;
+    background-color: rgb(21, 21, 21);
+}
+
+.nav-links a {
+    text-decoration: none;
+    font-size: 1.2rem;
+    color: white;
+}
+
+.nav-links a:not(:last-child) {
+    margin-right: 5rem;
+}
+
+.logo {
+    width: 4rem;
 }
 </style>
