aa37bd08fcad916352b5277df237420551e1b354
JosephOkpala <okpalajoseph995@gmail.com>
2023-03-14 17:19:14 +0100

    added router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../src/routes/HomeRoute.vue';
+import Repos from '../src/routes/ReposRoute.vue';
+import Profile from '../src/routes/ProfileRoutes.vue';
+import WildcardRouter from '../src/routes/Wildcard.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/repos',
+      name: 'Repos',
+      component: Repos,
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'WildcardRouter',
+      component: WildcardRouter,
+    },
+  ],
+});
+
+createApp(App).use(router).mount('#app');
