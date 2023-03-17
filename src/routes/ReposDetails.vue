7c8edbcf1454a37579d0e7ded161d031c0e28c7b
JosephOkpala <okpalajoseph995@gmail.com>
2023-03-17 02:15:54 +0100

    create nested route file

---
+++ src/routes/ReposDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>{{ repo.name }}</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default{
+    props: ['id'],
+    data(){
+        return{
+            repo: {},
+        };
+    },
+    mounted(){
+        axios
+        .get(`https://api.github.com/repositories/${this.id}`)
+        .then((response) =>{
+            this.repo = response.data
+        })
+    }
+}
+</script>
