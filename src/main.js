d4d87998847fd99b15ee0887b7f191bbf7832722
JosephOkpala <okpalajoseph995@gmail.com>
2023-03-17 02:15:33 +0100

    added nested routes component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Home from '../src/routes/HomeRoute.vue';
 import Repos from '../src/routes/ReposRoute.vue';
 import Profile from '../src/routes/ProfileRoutes.vue';
 import WildcardRouter from '../src/routes/Wildcard.vue';
+import ReposDetails from '../src/routes/ReposDetails.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -18,6 +19,11 @@ const router = createRouter({
       path: '/repos',
       name: 'Repos',
       component: Repos,
+    },
+    {
+      path: '/repos/:id',
+      component: ReposDetails,
+      props: true,
     },
     {
       path: '/profile',
