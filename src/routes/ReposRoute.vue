23b31118a507194c2bf1cbcb27b8af600bd74263
JosephOkpala <okpalajoseph995@gmail.com>
2023-03-18 10:00:11 +0100

    Pagination markup and styles

--- src/routes/ReposRoute.vue
+++ src/routes/ReposRoute.vue
@@ -1,5 +1,6 @@
 <template>
-    <div v-for="repo in repos" :key="repo.id" class="repoContainer">
+  <div class="container">
+    <div v-for="(repo) in paginatedRepos" :key="repo.id" class="repoContainer">
         <div class="each-repo">
             <img :src="repo.owner.avatar_url" alt="Profile picture" class="profile-img mg-1">
             <h1 class="mg-1">Project name: {{ repo.name }}</h1>
@@ -17,6 +18,13 @@
         <h1>Loading...</h1>
       </div>
     </div>
+    <div class="pagination">
+      <button :disabled="currentPage === 1" @click="currentPage--" class="prev-button pagination-button">Prev</button>
+      <button v-for="pageNumber in totalPages" :key="pageNumber"
+        :class="{active: pageNumber === currentPage}" @click="currentPage = pageNumber" class="pagination-button">{{ pageNumber }}</button>
+      <button :disabled="currentPage === totalPages" @click="currentPage++" class="next-button pagination-button">Next</button>
+    </div>
+  </div>
   </template>
   
   <script>
@@ -27,20 +35,54 @@
       return {
         repos: [],
         loading: true,
+        currentPage: 1,
+        perPage: 5,
       };
     },
-  
-    mounted() {
-      axios.get('https://api.github.com/users/JosephOkpala/repos').then((response) => {
+
+    computed: {
+      paginatedRepos(){
+        const startIndex = (this.currentPage - 1) * this.perPage;
+        const endIndex = startIndex + this.perPage;
+        return this.repos.slice(startIndex, endIndex);
+      },
+      totalPages(){
+        return Math.ceil(this.repos.length / this.perPage)
+      },
+    },
+
+    methods:{
+      fetchRepos(){
+        axios.get('https://api.github.com/users/JosephOkpala/repos').then((response) => {
         this.repos = response.data;
         this.loading= false;
         console.log(response);
       });
+      },
+    },
+  
+    mounted() {
+      this.fetchRepos();
+    },
+    watch: {
+      currentPage(newValue, oldValue){
+        if(newValue !== oldValue){
+          window.scrollTo({ 
+            top: 0,
+            behavior: 'smooth'
+          })
+        }
+      },
     },
   };
   </script>
 
 <style scoped>
+
+.container{
+  width: 100%;
+  margin-bottom: 2rem;
+}
 .repoContainer{
     width: 90%;
     max-width: 1660px;
@@ -76,5 +118,35 @@
     border-radius: 10rem;
     padding: .8rem .8rem;
     text-decoration: none;
+}
+
+.pagination{
+  border: 2px solid red;
+  text-align: center;
+  width: 90;
+  margin-top: 3rem auto 0rem auto;
+}
+
+.pagination button{
+  font-family: 'Merriweather', serif;
+  font-weight: 900;
+}
+
+.pagination button:not(:last-child){
+  margin-right: 0.5rem;
+}
+
+.pagination-button{
+  padding: .6rem 1rem;
+  border: none;
+}
+
+.prev-button:disabled{
+  background-color: grey;
+  cursor: not-allowed;
+}
+.next-button:disabled{
+  background-color: grey;
+  cursor: not-allowed;
 }
 </style>
